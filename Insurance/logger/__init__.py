e084774d72aedcd8a4dc5b38d42beb3913e9e58a
Ishan <ishanbakshi91@gmail.com>
2025-07-11 00:31:37 +0530

    utils file and config file updated

--- Insurance/logger/__init__.py
+++ Insurance/logger/__init__.py
@@ -23,7 +23,7 @@ logging.basicConfig(filename=LOG_FIlE_PATH,
 filemode = "w",
 format = '[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
 
-#level=logging.INFO,
-level=logging.DEBUG,
+level=logging.INFO,
+#level=logging.DEBUG,
 )
 
