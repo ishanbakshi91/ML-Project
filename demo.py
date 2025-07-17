5a2d3a9af0a52b413306ec05d1259a7aa3afd6d1
Ishan <ishanbakshi91@gmail.com>
2025-07-17 10:33:49 +0530

    batch prediction and training pipeline

---
+++ demo.py
@@ -0,0 +1,16 @@
+#Batch Prediction
+#Training Pipeline
+
+
+from Insurance.pipeline.batch_prediction import start_batch_prediction
+from Insurance.pipeline.training_pipeline import start_training_pipeline
+
+#file_path=r"F:\workspace-ishan\ML-Project\insurance.csv"
+print(__name__)
+if __name__=="__main__":
+    try:
+        output_file = start_training_pipeline()
+        #output_file = start_batch_prediction(input_file_path=file_path)
+        print(output_file)
+    except Exception as e:
+        print(e)
